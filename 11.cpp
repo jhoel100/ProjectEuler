3b623f2f028ad68b9ec5d3c77a6cdd560541a0fd
jhoel100 <49628552+jhoel100@users.noreply.github.com>
2019-04-17 11:01:24 -0500

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  const unsigned int Size = 20;
+  unsigned int matrix[Size][Size];
+
+  for (unsigned int y = 0; y < Size; y++){
+    for (unsigned int x = 0; x < Size; x++){
+      cout<<x<<""<<y;
+      cin >> matrix[x][y];}}
+
+  unsigned int best = 0;
+  for (unsigned int y = 0; y < Size; y++)
+    for (unsigned int x = 0; x < Size; x++)
+    {
+      if (x + 3 < Size)
+      {
+        unsigned int current = matrix[x][y] * matrix[x+1][y] * matrix[x+2][y] * matrix[x+3][y];
+        if (best < current)
+          best = current;
+      }
+      if (y + 3 < Size)
+      {
+        unsigned int current = matrix[x][y] * matrix[x][y+1] * matrix[x][y+2] * matrix[x][y+3];
+        if (best < current)
+          best = current;
+      }
+      if (x + 3 < Size && y + 3 < Size)
+      {
+        unsigned int current = matrix[x][y] * matrix[x+1][y+1] * matrix[x+2][y+2] * matrix[x+3][y+3];
+        if (best < current)
+          best = current;
+      }
+      if (x + 3 < Size && y >= 3)
+      {
+        unsigned int current = matrix[x][y] * matrix[x+1][y-1] * matrix[x+2][y-2] * matrix[x+3][y-3];
+        if (best < current)
+          best = current;
+      }
+    }
+
+  cout << best << endl;
+  return 0;
+}
---
+++ 12.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+  const unsigned int MaxDivisors = 1000;
+
+  vector<unsigned int> smallest;
+  smallest.push_back(0);
+
+  unsigned int index    = 0;
+  unsigned int triangle = 0; 
+  while (smallest.size() < MaxDivisors)
+  {
+    index++;
+    triangle += index;
+
+    if (smallest.size() > 300 && triangle % 10 != 0)
+      continue;
+
+    unsigned int divisors = 0;
+    unsigned int i        = 1;
+    while (i*i < triangle)
+    {
+      if (triangle % i == 0)
+        divisors += 2;
+      i++;
+    }
+    if (i*i == triangle)
+      divisors++;
+
+    while (smallest.size() <= divisors)
+      smallest.push_back(triangle);
+  }
+
+    unsigned int minDivisors=500;
+
+    cout << smallest[minDivisors + 1] << endl;
+ 
+
+  return 0;
+}
