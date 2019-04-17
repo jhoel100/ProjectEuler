fd661c930564c24512163b4c0724cc1ca0b0b61b
jhoel100 <49628552+jhoel100@users.noreply.github.com>
2019-04-17 11:13:19 -0500

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -18,30 +18,34 @@ int main()
     {
       if (x + 3 < Size)
       {
+        //verificar en linea recta
         unsigned int current = matrix[x][y] * matrix[x+1][y] * matrix[x+2][y] * matrix[x+3][y];
         if (best < current)
           best = current;
       }
       if (y + 3 < Size)
       {
+        //verificar en linea vertical
         unsigned int current = matrix[x][y] * matrix[x][y+1] * matrix[x][y+2] * matrix[x][y+3];
         if (best < current)
           best = current;
       }
       if (x + 3 < Size && y + 3 < Size)
       {
+        //verificar en diagonal para abajo
         unsigned int current = matrix[x][y] * matrix[x+1][y+1] * matrix[x+2][y+2] * matrix[x+3][y+3];
         if (best < current)
           best = current;
       }
       if (x + 3 < Size && y >= 3)
       {
+        //verificar en diagonal para arriba
         unsigned int current = matrix[x][y] * matrix[x+1][y-1] * matrix[x+2][y-2] * matrix[x+3][y-3];
         if (best < current)
           best = current;
       }
     }
 
-  cout << best << endl;
+  cout <<"La solucion es de: "<< best << endl;
   return 0;
 }
