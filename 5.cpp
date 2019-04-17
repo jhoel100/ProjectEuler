79e5c1b551ef1b4c07a35b11990369279478f1e2
jhoel100 <49628552+jhoel100@users.noreply.github.com>
2019-04-17 11:41:41 -0500

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,10 +1,11 @@
+//hallar el menor numero que es divisible entre los numeros del 1 al 20
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	unsigned long i=2;
+	unsigned long i=20;
 	while(true){
 		if((i%2==0)&&(i%3==0)&&(i%4==0)&&(i%5==0)&&(i%6==0)&&(i%7==0)&&(i%8==0)&&(i%9==0)&&(i%10==0)&&(i%11==0)&&(i%12==0)&&(i%13==0)&&(i%14==0)&&(i%15==0)&&(i%16==0)&&(i%17==0)&&(i%18==0)&&(i%19==0)&&(i%20==0)){
 			cout<<"    "<<i<<"   ";
