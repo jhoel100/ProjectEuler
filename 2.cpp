8d63f3e46a5d51d18ca5400e4abbe7933b027db4
jhoel100 <49628552+jhoel100@users.noreply.github.com>
2019-04-17 11:52:49 -0500

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main(){
 	unsigned int aux,x=1,y=2,rpta=0;
+	//halla el siguiente termino de fibonaci
 	while(y<4000000){
 		aux=y;
 		y=x+y;
 		x=aux;
+		//nos pide sumar solo los de posicion par
 		if(x%2==0){
 			rpta+=x;
 		}
