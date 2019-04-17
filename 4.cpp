fda77064dc55267be61555843a393792f0720f38
jhoel100 <49628552+jhoel100@users.noreply.github.com>
2019-04-17 11:49:25 -0500

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,4 +1,4 @@
-
+//https://euler.stephan-brumme.com
 #include <iostream>
 
 using namespace std;
@@ -14,16 +14,16 @@ unsigned int encpalindromo(unsigned int x)
 
 int main()
 {
-
+    //el producto de numeros de 3 no pasa los 6 digitos
     unsigned int limite=999999;
     bool bandera = false;
-
+    //comienza a probar con los valores de limite entre 1000 osea 3 digitos
     for (unsigned int inicio = limite / 1000; inicio >= 100 && !bandera; inicio--)
     {
       unsigned int palindromo = encpalindromo(inicio);
       if (palindromo >= limite)
         continue;
-
+      //verifica si tiene los 3 multiplos de 3 digitos
       for (unsigned int i = 100; i * i <= palindromo; i++)
         if (palindromo % i == 0)
         {
