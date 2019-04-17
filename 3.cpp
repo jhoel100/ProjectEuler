80fb8e23b024804f2efdae0d4fdf850babb2994a
jhoel100 <49628552+jhoel100@users.noreply.github.com>
2019-04-17 11:51:00 -0500

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -8,6 +8,7 @@ int main()
     unsigned long long x=600851475143;
 
     for (unsigned long long factor = 2; factor * factor <= x; factor++){
+      //encuentra un factor,si lo halla lo divide y reemplaza
       while (x % factor == 0 && x != factor) {
         x /= factor;
 	cout<<"factor: "<<factor<<endl; }
